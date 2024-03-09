875efb054a337bdb36ba893e87ee4f61a2a82419
Mikey Dowsett <mdowset1@stumail.jccc.edu>
2024-03-09 08:15:19 -0600

    Added new attempts options

--- SendTracker/obj/Debug/net8.0-android/SendTracker.AssemblyInfo.cs
+++ SendTracker/obj/Debug/net8.0-android/SendTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SendTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b1ec27be19a1fb5adb2c594218143f7898205b83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+52857080823c8321258c7233d237edd112676de9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SendTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SendTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
